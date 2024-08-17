57aa051b1ee685d1fb72247c396687376082044a
Ivan Timoshenko <timoshenko_is@remi.ru>
2024-08-17 17:34:27 +0400

    Rename and implement random local photo model
    Renamed `external` service to `random_local_photo` model and implemented `RandomLocalPhotoGenerativeModel`. Updated abstract method in `GenerativeModel` to include a prompt argument.

---
+++ src/pygmalion/models/random_local_photo.py
@@ -0,0 +1,20 @@
+from os import PathLike
+from pathlib import Path
+import random
+
+from pygmalion.engine.generative_model import GenerativeModel
+
+
+class RandomLocalPhotoGenerativeModel(GenerativeModel):
+    def __init__(self, photos_path: str | PathLike):
+        super().__init__()
+        self.photos_path = photos_path
+        self.photos_list = self._get_list_of_photos()
+
+    def _get_list_of_photos(self) -> list[str]:
+        path = Path(self.photos_path).glob('**/*.{png,gif,jpg,jpeg}')
+        files = [str(x) for x in path if x.is_file()]
+        return files
+
+    def generate_photo(self, prompt: str) -> str:
+        return random.choice(self.photos_list)
